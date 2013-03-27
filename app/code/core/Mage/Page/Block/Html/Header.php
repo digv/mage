8d6e4bf08d3f059735fc6f7433545ac4682317b3
gefferey <jhzhang@astreetpress.com>
2013-03-27 12:00:15 +0800

    translate

--- app/code/core/Mage/Page/Block/Html/Header.php
+++ app/code/core/Mage/Page/Block/Html/Header.php
@@ -77,7 +77,7 @@ class Mage_Page_Block_Html_Header extends Mage_Core_Block_Template
             if (Mage::isInstalled() && Mage::getSingleton('customer/session')->isLoggedIn()) {
                 $this->_data['welcome'] = $this->__('Welcome, %s!', $this->escapeHtml(Mage::getSingleton('customer/session')->getCustomer()->getName()));
             } else {
-                $this->_data['welcome'] = Mage::getStoreConfig('design/header/welcome');
+                $this->_data['welcome'] = $this->__(Mage::getStoreConfig('design/header/welcome'));
             }
         }
 
