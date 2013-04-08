d6ffac98298e5d441036b9416838513fc8a96c87
gefferey <jhzhang@astreetpress.com>
2013-04-08 22:23:38 +0800

    dd

--- app/code/community/Digv/Tenpaywarrant/controllers/PayController.php
+++ app/code/community/Digv/Tenpaywarrant/controllers/PayController.php
@@ -1,5 +1,5 @@
 <?php
-class Dig_Cms_PayController extends Mage_Core_Controller_Front_Action {
+class Digv_Tenpaywarrant_PayController extends Mage_Core_Controller_Front_Action {
 	
 	public function indexAction () {
 		echo 'pay';
